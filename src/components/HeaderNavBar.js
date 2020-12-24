823437148c6eb7ea9a4cd39dc856c493f81e8af0
Maxim Konstantinov <konstantinovmax@gmail.com>
2020-12-24 14:03:34 +0300

    feat: поменять роуты

--- src/components/HeaderNavBar.js
+++ src/components/HeaderNavBar.js
@@ -5,7 +5,7 @@ function HeaderNavBar() {
   return (
     <nav className="header__links">
         <ul className="header__menu">
-            <li className="header__menu-item"><NavLink exact to="/" className="header__link" activeClassName="header__link_active">Главная</NavLink></li>
+            <li className="header__menu-item"><NavLink exact to="*" className="header__link" activeClassName="header__link_active">О компании</NavLink></li>
             <li className="header__menu-item"><NavLink to="/chat-bot" className="header__link" activeClassName="header__link_active">Подать инициативу</NavLink></li>
             <li className="header__menu-item"><NavLink to="*" className="header__link" activeClassName="header__link_active">Контакты</NavLink></li>
         </ul> 
--- src/components/MenuMobile.js
+++ src/components/MenuMobile.js
@@ -5,7 +5,7 @@ function MenuMobile(props) {
     return (
         <>
             <ul className={`menu-mobile ${props.isOpen && 'menu-mobile_is-open'}`}>
-                <li className="menu-mobile__item" onClick={props.onClose}><NavLink exact to="/" className="menu-mobile__link">Главная</NavLink></li>
+                <li className="menu-mobile__item" onClick={props.onClose}><NavLink exact to="*" className="menu-mobile__link">О компании</NavLink></li>
                 <li className="menu-mobile__item" onClick={props.onClose}><NavLink to="/chat-bot" className="menu-mobile__link">Подать инициативу</NavLink></li>
                 <li className="menu-mobile__item" onClick={props.onClose}><NavLink to="*" className="menu-mobile__link">Контакты</NavLink></li>
                 <button className="menu-mobile__close-button-container" onClick={props.onClose}>
